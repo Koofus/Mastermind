9f1c217d099c5e915113c169d2af730559bcd29c
ryan-weatherreport <43148291+ryan-weatherreport@users.noreply.github.com>
2018-09-10 13:33:33 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std; // replace with individual std imports like using std::cout for opptimizatoin
+
+class Color {
+	public
+		Color(string a) {
+			color = a;
+		}
+	
+	private:
+		string color = "Red;";
+		
+}
+
+class Seires {
+	public:
+		Compare(Series a)
+		
+};
+
+int a[] = {true, false, false true }
+
+
+int main() {
+	
+	
+	
+}
